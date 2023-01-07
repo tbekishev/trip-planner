a29d7c1157bad4f851c7fcdd8aef3f24be5867b6
Rouzbeh-Berenji <berenji.ro@gmail.com>
2023-01-07 01:12:49 -0500

    Creating database Tables

---
+++ server/db/schema/03-attractions.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate attractions table
+
+DROP TABLE IF EXISTS attractions CASCADE;
+CREATE TABLE attractions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  city VARCHAR(255),
+  rate FLOAT,
+  average_budget FLOAT
+);
