fa24df9696137225a4b48170074a520396025a4d
Tuigun Bekishev <tbekishev@gmail.com>
2023-01-07 14:31:27 -0400

    install pg, bcrypt/add registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'travel'
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  pool.query(` INSERT INTO users (first_name, last_name, email, password)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;`, [firstName, lastName, email, password]
+  )
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log("Error: ",err.message);
+  });
+})
 module.exports = router;
