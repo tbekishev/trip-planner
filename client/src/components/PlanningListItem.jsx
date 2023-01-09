8dcc3886cea1121370c8c80589c4d4e848a14503
RuoningJin <ronjin2013@hotmail.com>
2023-01-08 19:14:02 -0500

    fixed the bug of header not flowing on top

--- client/src/components/PlanningListItem.jsx
+++ client/src/components/PlanningListItem.jsx
@@ -15,7 +15,7 @@ export default function PlanningListItem() {
       direction={{ base: 'column', sm: 'row' }}
       overflow='hidden'
       variant='outline'
-      className='planning-items'
+      className='planning-item'
     >
       <img
         src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
@@ -24,16 +24,16 @@ export default function PlanningListItem() {
 
       <Stack>
         <CardBody>
-          <Heading size='md'>Trip 1</Heading>
+          <Heading size='md'>Graduation Trip</Heading>
 
-          <Text py='2'>
+          <Text className='planning-item--discription' py='2'>
             Caffè latte is a coffee beverage of Italian origin made with espresso
             and steamed milk.
           </Text>
         </CardBody>
         
-        <CardFooter className='planning-items--footer'>
-          4 days | $2000 | Group of 4 | @Toronto
+        <CardFooter className='planning-item--footer'>
+          4 days | $2000 | Group of 4 | @Toronto, Canada
         </CardFooter>
       </Stack>
     </Card>
