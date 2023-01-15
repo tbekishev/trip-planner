86e2b18aeaa9a2d452af57f4deaae079dbae577b
Tuigun Bekishev <tbekishev@gmail.com>
2023-01-15 18:52:19 -0400

    add circle loading progress icon, change select to radio

--- client/src/components/Places/List.jsx
+++ client/src/components/Places/List.jsx
@@ -1,45 +1,54 @@
 import ListItems from './ListItems';
-import { Box, CircularProgress, FormControl, FormLabel, Select, Text } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Box, CircularProgress, FormControl, FormLabel, Radio, RadioGroup, Stack } from '@chakra-ui/react';
+import './List.scss';
 
 export default function List(props) {
 
-  const isLoading = false;
-  const [rating, setRating] = useState('');
   return (
     <Box style={{padding: '25px'}}>
-      <Text fontSize="xl">Attractions, Accomodations & Dining around you</Text>
-      {isLoading ? (
+
+        <section className='top-container'>
+          <div className='filter'>
+
+          <FormControl className='filter-item'>
+            <FormLabel>Type</FormLabel>
+            <RadioGroup value={props.type} onChange={props.setType}>
+              <Stack direction="row">
+                <Radio value="attractions">Attractions</Radio>
+                <Radio value="restaurants">Restaurants</Radio>
+                <Radio value="hotels">Hotels</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+
+            <FormControl className='filter-item'>
+              <FormLabel>Rating</FormLabel>
+              <RadioGroup value={props.rating} onChange={props.setRating}>
+              <Stack direction="row">
+                <Radio value="">All</Radio>
+                <Radio value="3">Above 3.0</Radio>
+                <Radio value="4">Above 4.0</Radio>
+                <Radio value="4.5">Above 4.5</Radio>
+              </Stack>
+              </RadioGroup>
+            </FormControl> 
+          </div>
+
+        </section>
+      {props.isLoading ? (
         <Box justifyContent="center" alignItems="center" d="flex">
           <CircularProgress isIndeterminate color="teal" size="5rem" />
         </Box>
       ) : (
-      <>
-        <FormControl style={{margin: '1rem', minWidth: 120, marginBottom: '30px'}}>
-          <FormLabel>Type</FormLabel>
-          <Select value={props.type} onChange={(e) => props.setType(e.target.value)}>
-            <option value="restaurants">Restaurants</option>
-            <option value="hotels">Hotels</option>
-            <option value="attractions">Attractions</option>
-          </Select>
-        </FormControl>
-        <FormControl style={{margin: '1rem', minWidth: 120, marginBottom: '30px'}}>
-          <FormLabel>Rating</FormLabel>
-          <Select value={rating} onChange={(e) => setRating(e.target.value)}>
-            <option value="">All</option>
-            <option value="3">Above 3.0</option>
-            <option value="4">Above 4.0</option>
-            <option value="4.5">Above 4.5</option>
-          </Select>
-        </FormControl> 
+
+
         <Box display="flex" flexWrap="wrap" style={{height: '75vh', overflow: 'auto'}}>
            {props.places?.map((place, i) => (
             <Box key={i} p={2}>
               <ListItems place={place} />
             </Box>
           ))}
-        </Box>       
-      </>
+        </Box> 
       )}
     </Box>
   );
